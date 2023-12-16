02555ea193355901a4d06124ee774c9a6d2cf672
heberth18 <heberthcaripa@gmail.com>
2023-12-16 08:58:40 -0300

    Agregué la variable booleana

--- misScripts.js
+++ misScripts.js
@@ -7,17 +7,17 @@ let boton = document.getElementById('miBoton');
 let archivo = 'peliculas.json'; // variable que va aparecer por defecto
 
 //ESCUCHADORES  DE EVENTOS
-selector.addEventListener('change', cambiarArchivo)// Chage es un evento propio de los selectores
+selector.addEventListener('change', cambiarArchivo)
 selector.addEventListener('cambioModo', mensajeModo);
-input.addEventListener('keydown', verificarInput);
-boton.addEventListener('click', buscar);
+input.addEventListener('keydown', verificarInput);; 
+boton.addEventListener('click', buscar);    
 
 
 //FUNCIONES 
 function cambiarArchivo(){
     archivo = selector.value;
     let evento = new CustomEvent('cambioModo');// Se crea evento personalizado
-    selector.dispatchEvent(evento);// se dispara el evento
+    selector.dispatchEvent(evento);
 }
 
 function mensajeModo(){
@@ -28,22 +28,39 @@ function verificarInput(evento){
     if((evento.keyCode < 65 || evento.keyCode > 90) && evento.keyCode != 32 && evento.keyCode != 8){
         evento.preventDefault();
     }
+
+    if(evento.key === "Enter"){
+        buscar();
+    }
+
 }
 
+
 function buscar(){
-    lista.innerHTML = "";// Cada vez que presione el boton este quedara vacio
+    lista.innerHTML = "";
 
     fetch(archivo)
     .then(respuesta => respuesta.json())
     .then(function(salida){
+
+        let seEncontroCoincidencia = false;
+
         for(let item of salida.data){
+            console.log('Item For:', item.nombre);
+
             if(item.nombre.startsWith(input.value.toUpperCase())){
+            
+
+                seEncontroCoincidencia = true;
+
+                console.log("Este es el if");
                 let p = document.createElement('p');
                 p.id = item.nombre;
-                p.innerHTML = item.sinopsis;// se le agrega al parrafo el contenido de la sinopsis
-                p.style.display = 'none';// Aparecen los archivos sin la sinopsis
+                p.innerHTML = item.sinopsis;
+                p.style.display = 'none';
 
                 let li = document.createElement('li');
+                console.log("Creando lista");
                 li.innerHTML = item.nombre;
                 li.addEventListener('mouseover', function(){
                     let p = document.getElementById(item.nombre);
@@ -56,11 +73,19 @@ function buscar(){
                 })
 
                 li.appendChild(p);
+                console.log("Hijos");
                 lista.appendChild(li);
+                
             }
         }
+        
+        if(!seEncontroCoincidencia){
+            console.log("No se encontraron coincidencias")
+            alert( '"'+input.value+'"' + " no se encuentra en cartelera");
+        }
+
     })  
     .catch(function(error){
-        console.log(error);
+        console.log('Error', error);
     })
 }
